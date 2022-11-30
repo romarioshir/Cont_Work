ea218e5bdc13effee0fa19c7e812c39814e36dac
romarioshir <roma-roman26@yandex.ru>
2022-11-30 18:58:33 +0300

    Added method SecondArray

--- Task/Program.cs
+++ Task/Program.cs
@@ -1,2 +1,14 @@
-﻿string[] array1 = new string[5] {"Moscow" , "134", "Pow", "world", "No"};
+﻿string[] array1 = new string[5] { "Moscow", "134", "Pow", "world", "No" };
 string[] array2 = new string[array1.Length];
+void SecondArray(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+        if (array1[i].Length <= 3)
+        {
+            array2[count] = array1[i];
+            count++;
+        }
+    }
+}
